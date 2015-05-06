c782bfbf2241a45dbf6233fefe06cf9dfe363e6c
indy9000 <indy9000@gmail.com>
2015-05-06 23:52:29 +0100

    Script to mirror the console output - first commit

---
+++ MirrorConsole/MirrorConsole.fsx
@@ -0,0 +1,47 @@
+﻿open System
+open System.IO
+
+type TwinWriter(t1:TextWriter, t2:TextWriter) =
+    inherit TextWriter()
+
+    let textWriter1 = t1
+    let textWriter2 = t2
+
+    override u.Encoding with get() = t1.Encoding
+    override u.Flush() = t1.Flush();  t2.Flush()
+    override u.Write(c:char) = t1.Write(c); t2.Write(c)
+
+type MirrorConsole(outputPath:string) as self =
+    
+    let filename = sprintf "console.mirror-%s.txt" (DateTime.UtcNow.ToString("yyyyMMddTHHmmss.fffffff"))
+    let pathFileName = System.IO.Path.Combine(outputPath,filename)
+    let fWriter = File.CreateText(pathFileName)
+    let stdOut = Console.Out
+    do
+        fWriter.AutoFlush <- true
+        let tw = new TwinWriter(TextWriter.Synchronized(fWriter),stdOut)
+        Console.SetOut(tw)
+
+    interface System.IDisposable with
+        member this.Dispose() = 
+            Console.SetOut(stdOut)
+            fWriter.Dispose()
+
+    member this.Close() =
+        self :> IDisposable |> fun k-> k.Dispose()
+
+(**
+Usage
+
+let mirror = new MirrorConsole(@"c:\temp\")
+
+Console.WriteLine("before Hello World Interactive")
+printfn "before Hello printfn"
+
+[|0..1000|] |> Array.Parallel.iteri(fun i k -> printfn "[%d] %d" i (k * k))
+
+mirror.Close()
+
+Console.WriteLine("after Hello World Interactive")
+printfn "after Hello printfn"
+*)
